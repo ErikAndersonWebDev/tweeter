b40cd04143ca4ce6916a990eb9b56e5e9cfeccb6
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-02-01 15:14:09 -0800

    Post stored tweets in template

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,37 +1,67 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(() => {
 
-// get name
-// get avatar
-// get handle
-// get text
-// get date
-
-
-/*
-
-const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+   {
+     "user": {
+       "name": "Newton",
+       "avatars": "https://i.imgur.com/73hZDYK.png",
+       "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-*/
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+        "content": {
+          "text": "Je pense , donc je suis"
+        },
+      "created_at": 1461113959088
+      }
+    ]
+    
+    
+    const createTweetElement = function(obj) {
+      const name = obj.user.name;
+      const handle = obj.user.handle;
+      const text = obj.content.text;
+      const dateCreated = obj.created_at;
+      const $tweet = $(`
+        <article class="tweet">
+          <header class="tweet">
+            <div class="pro-details">
+              <img src="/images/profile-hex.png">
+              <p>${name}</p>
+            </div>
+            <p>${handle}</p>
+          </header>
+          <div class="posted-tweet">
+            ${text}
+          </div>
+          <footer class="tweet">
+            Posted ${dateCreated}
+            <div>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+      `)
+      return $tweet;
+    }
+    
+    const renderTweets = function(tweets) {
+      for (let tw of tweets) {
+        const $createdTweet = createTweetElement(tw);
+        $('#tweets-container').append($createdTweet); 
+      }
+    }
+    
+    renderTweets(data);
+    
+  });
