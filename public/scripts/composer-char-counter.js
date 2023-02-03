8fbd74e79a58996a706ca7083e577d23e110b0bc
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-02-03 15:24:14 -0800

    Update counter variable path

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 // Tweet character counter - default 140 - black-color when above 0, red-color when below 0
   $('textarea').keyup(function(){
     const charCount = $(this).val().length;
-    const counter = $(this).parent().children().children('.counter');
+    const counter = $(this).closest("form").find(".counter");
     counter.text(140 - charCount);
     if (charCount > 140) {
       counter.addClass('negative')
